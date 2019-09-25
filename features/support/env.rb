ef1e4d3085fbc8bb8e05ef1e8ae0228decaec236
marisleidy <marisleidy.alex@gmail.com>
2019-09-25 13:56:50 -0500

    prueba

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,3 +12,21 @@ require 'statsd-ruby'
 require 'time'
 require 'base64'
 require 'report_builder'
+
+Before do //se ejecuta ante cada escenario////siempre inicias el driver de watir que es el que levanta el navegador//
+    $driver =  Watir::Browser.new :firefox
+
+end
+
+
+After do //se ejecuta despues de cada Scenario//
+
+    $driver.close
+
+end
+
+
+at_exit do //al final de todo//
+
+
+end
